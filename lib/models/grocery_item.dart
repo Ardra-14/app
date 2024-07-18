5382f90ac2c2e0b1cb55a8ed31c0b55b79b2af0c
Ardra <ardravijayakumar14@gmail.com>
2024-07-18 19:38:38 +0530

    commit

--- lib/models/grocery_item.dart
+++ lib/models/grocery_item.dart
@@ -1,117 +1,14 @@
 import 'package:flutter/material.dart';
 
-
-class Grocery{
+class Grocery {
   final int id;
   final String name;
   final double price;
   final String imagePath;
 
-  Grocery({
-    required this.id, 
-    required this.name, 
-    required this.price, 
-    required this.imagePath
-    });
+  Grocery(
+      {required this.id,
+      required this.name,
+      required this.price,
+      required this.imagePath});
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class GroceryItems {
-//   final String id;
-//   final String name;
-//   final double price;
-//   final String imagePath;
-//   int quantity;
-  
-
-//   GroceryItems({
-    
-//     required this.id,
-//     required this.name,
-//     required this.price,
-//     required this.imagePath,
-//     this.quantity = 1,
-    
-//   });
-
-// }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       decoration: BoxDecoration(
-//         color: Colors.white,
-//         border: Border.all(
-//           color: Color.fromARGB(255, 218, 216, 216),
-//           width: 1,
-//         ),
-//         borderRadius: BorderRadius.circular(10),
-//       ),
-//       child: Column(
-//         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//         children: [
-//           //image
-
-//           Image.asset(
-//             imagePath,
-//             height: 64,
-//           ),
-
-//           //itemname
-//           Text(
-//             name,
-//             style: TextStyle(
-//               color: Colors.black,
-//               fontSize: 15,
-//               fontWeight: FontWeight.bold,
-//             ),
-//           ),
-//           //price
-
-//           Text(
-//             price,
-//             style: TextStyle(
-//               color: Colors.black,
-//               fontSize: 15,
-//               fontWeight: FontWeight.bold,
-//             ),
-//           ),
-
-//           //add to cart button
-
-//           MaterialButton(
-//             padding: EdgeInsets.all(18),
-//             onPressed: onPressed,
-//             color: Colors.green[100],
-//             child: Text(
-//               'Add to Cart',
-//               style: TextStyle(
-//                   color: Colors.black,
-//                   fontWeight: FontWeight.bold,
-//                   fontSize: 15),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
